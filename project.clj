a95b6476096c737c6e37b29dcf641304b0762409
Pedro Delfino <pedro@Pedros-MacBook-Air.local>
2023-01-05 19:27:48 -0300

    project.clj: temporary tweak to publish a Maven package

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.10.126"]]
-  :plugins [[lein-cljsbuild "1.1.8"]]
+  :plugins [[lein-cljsbuild "1.1.8"]
+            [lein-bump-version "0.1.6"]
+            [lein-tools-deps "0.4.5"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  ;; run lein install with LEIN_SNAPSHOTS_IN_RELEASE=true lein install
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:id "whitespace"
                         :source-paths ["src" "test"]
@@ -14,9 +19,22 @@
                                    :pretty-print true}}
                        {:id "simple"
                         :source-paths ["src" "test"]
-                        :compiler {:output-to "target/js/simple.js"
-                                   :optimizations :simple
-                                   :pretty-print true}}
+                        ;; Change your environment variables (maybe editing .zshrc or .bashrc) to have:
+                        ;; export LEIN_USERNAME="pdelfino"
+                        ;; export LEIN_PASSWORD="your-personal-access-token-the-same-used-on-.npmrc"
+                        ;; LEIN_PASSWORD should use the same Token used by .npmrc
+                        ;; Also, do "LEIN_SNAPSHOTS_IN_RELEASE=true lein install" or edit your .zshrc:
+                        ;; export LEIN_SNAPSHOTS_IN_RELEASE=true
+                        :repositories {"releases"  {:url           "https://maven.pkg.github.com/tallyfor/*"
+                                                    :username      :env/LEIN_USERNAME ;; change your env
+                                                    :password      :env/LEIN_PASSWORD}}
+
+                        :pom-addition [:distribution-management [:repository [:id "github"]
+                                                                 [:name "GitHub Packages"]
+                                                                 [:url "https://maven.pkg.github.com/tallyfor/dataflow"]]]
+                                              :compiler {:output-to "target/js/simple.js"
+                                                         :optimizations :simple
+                                                         :pretty-print true}}
                        {:id "advanced"
                         :source-paths ["src" "test"]
                         :compiler {:output-to "target/js/advanced.js"
